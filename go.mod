2d4374af64bf8c0b77d192dbd51c9b22fb93e25a
mHALr <ryo97321@gmail.com>
2020-09-06 14:56:58 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ryo97321/image
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,39 @@
+package image
+
+import (
+	"image"
+	"image/jpeg"
+	"image/png"
+	"os"
+	"strings"
+)
+
+// ChangeImageExtension srcFileExtの拡張子からdstFileExtの拡張子に変換する
+func ChangeImageExtension(imageFilePath string, srcFileExt string, dstFileExt string) error {
+	imageFile, err := os.Open(imageFilePath)
+	if err != nil {
+		return err
+	}
+	defer imageFile.Close()
+
+	img, _, err := image.Decode(imageFile)
+	if err != nil {
+		return err
+	}
+
+	dstImageFilePath := strings.Trim(imageFilePath, srcFileExt) + dstFileExt // 変換後の画像ファイルのパス
+
+	dstImageFile, err := os.Create(dstImageFilePath)
+	if err != nil {
+		return err
+	}
+	defer dstImageFile.Close()
+
+	if dstFileExt == ".jpg" {
+		jpeg.Encode(dstImageFile, img, nil) // jpegに変換
+	} else if dstFileExt == ".png" {
+		png.Encode(dstImageFile, img) // pngに変換
+	}
+
+	return nil
+}
